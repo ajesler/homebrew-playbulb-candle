12eb3bac399287dd46174d5c231b3684adb2b8d4
AJ Esler <aj@hatchinvest.nz>
2019-09-17 23:23:50 +1200

    Install playbulb-candle v0.2.0

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # homebrew-playbulb-candle
 
 Installs the [candle-cli](https://github.com/ajesler/playbulb-candle) binary for controlling MiPOW Playbulb candles via the comamnd line.
+
+
+### Releasing a new version
+
+Assuming you are running on macOS,
+
+1. Tag a new release of https://github.com/ajesler/playbulb-candle with a version, eg v0.2.0
+1. Go to the [releases page](https://github.com/ajesler/playbulb-candle/releases) and download the .tar.gz file
+1. run `shasum -a 256 playbulb-candle-0.2.0.tar.gz`
+1. Update `playbulb-candle.rb` with a link to the new tar.gz file and update the sha256 value
+1. Run `brew install --build-from-source ./playbulb-candle.rb` to check it installs
--- playbulb-candle.rb
+++ playbulb-candle.rb
@@ -3,17 +3,12 @@ require "language/go"
 class PlaybulbCandle < Formula
   desc "Control MiPOW Playbulb Candles from the terminal"
   homepage "https://github.com/ajesler/playbulb-candle"
-  url "https://github.com/ajesler/playbulb-candle/archive/v0.1.1.tar.gz"
-  sha256 "db82d6b5ccfc4bc779efb7f47c748898380a620113122cbc13c08c79ab5627ba"
+  url "https://github.com/ajesler/playbulb-candle/archive/v0.2.0.tar.gz"
+  sha256 "4952e8b80bfbbf140a9361b19d65be14fc3cedd229e64443bfd0e291c39500f5"
 
   head "https://github.com/ajesler/playbulb-candle.git"
 
   depends_on "go" => :build
-
-  go_resource "github.com/currantlabs/gatt" do
-    url "https://github.com/currantlabs/gatt.git",
-    :revision => "f949eac78f4ebdd9793c441ea7a7ff330eb323b0"
-  end
 
   def install
     ENV["GOPATH"] = buildpath
